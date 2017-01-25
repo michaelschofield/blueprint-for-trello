69b512f97ba59803eee15428847c63709a7ad383
michaelschofield <michael@schoeyfield.com>
2017-01-25 01:11:29 -0500

    Removes the jquery dependency, and fixes major usability issues.

--- blueprint.js
+++ blueprint.js
@@ -1,43 +1,94 @@
 
-var blueprintLayers = [ '[touchpoint]', '[actor]', '[critical]', '[data]', '[idea]', '[observation]', '[policy]', '[question]', '[stakeholder]', '[system]' ];
+const blueprintLayers = [ '[touchpoint]', '[actor]', '[critical]', '[data]', '[idea]', '[observation]', '[policy]', '[question]', '[stakeholder]', '[system]' ];
 
-var blueprintify = function() {
+const blueprintify = function() {
 
-  $( '.list-card' ).each( function( index ){
+    const cards = document.querySelectorAll( '.list-card' );
+    const lists = document.querySelectorAll( '.list' );
 
-    var card  = this,
-        icon,
-        title = card.querySelector( '.list-card-title' ),
-        layers = ( title.textContent.match(/\[([^)]+)\]/) ? title.textContent.match(/\[([^)]+)\]/)  : '') ;
+    cards.forEach( card => {
 
-    if ( $.inArray( layers[0], blueprintLayers ) > -1 ) {
+        const title = card.querySelector( '.list-card-title' );
+        const layers = ( title ? title.textContent.match(/\[([^)]+)\]/gi)  : '');
 
-      var layer = layers[0].replace(/[\[\]']+/g,'');
+        if ( layers && blueprintLayers.includes( layers[0].toLowerCase() ) ) {
 
-      icon = document.createElement( 'img' );
-      icon.setAttribute( 'src', chrome.extension.getURL( '/images/' + layer + '.svg' ) );
-      icon.classList.add( 'step__icon' );
+            const layer = layers[0].replace(/[\[\]']+/g,'').toLowerCase();
+            const icon = document.createElement( 'img' );
 
-      // innerHTML rather than textContent preserves some of the hidden
-      // elements trello bakes-in.
-      title.innerHTML = title.innerHTML.replace( layers[0], '' );
-      card.classList.add( 'step__' + layer );
-      $( title ).prepend( icon );
+            icon.setAttribute( 'src', chrome.extension.getURL( '/images/' + layer + '.svg' ) );
+            icon.classList.add( 'step__icon' );
+            title.innerHTML = title.innerHTML.replace( layers[0], '' );
+            card.classList.add( 'step__' + layer );
 
-    }
+            card.querySelector( '.list-card-details').insertBefore( icon, title );
 
-  });
+        }
+
+    });
+
+    lists.forEach( list => {
+       const step =  list.querySelector( 'h2.list-header-name-assist' );
+       const visibility = ( step ? step.textContent.match(/\[([^)]+)\]/)  : '');
+
+       if ( visibility && visibility[0].toLowerCase() == '[hidden]' ) {
+           list.classList.add( 'step--hidden' );
+           step.innerHTML = step.innerHTML.replace( visibility[0], '' );
+       }
+    });
+
 }
 
-//what to do when DOM loads
-$(function(){
+const CrossBrowser = {
+    init: function(){
+        this.MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver || null;
+    }
+};
 
-	$('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').off('mouseup');
-	$('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').on('mouseup', blueprintify);
-	$('.js-input').off('keyup');
-	$('.js-input').on('keyup', blueprintify);
-	$('.js-share').off('mouseup');
+CrossBrowser.init();
+
+const listObserver = new CrossBrowser.MutationObserver( function( mutations ) {
+
+    mutations.forEach( function( mutation, index ) {
+
+        const $target = mutation.target;
+
+        if ( $target.classList ) {
 
 
-	blueprintify();
+            if (!($target.classList.contains('list-total')
+                    || $target.classList.contains('list-title')
+                    || $target.classList.contains('list-header')
+                    || $target.classList.contains('date') // the 'time-ago' functionality changes date spans every minute
+                    || $target.classList.contains('js-phrase') // this is constantly updated by Trello, but doesn't affect estimates.
+                    || $target.classList.contains('member')
+                    || $target.classList.contains('clearfix')
+                    || $target.classList.contains('badges')
+                    || $target.classList.contains('header-btn-text')
+                    || (typeof mutation.target.className == "undefined")
+                )) {
+                if ($target.classList.contains('badge')) {
+                    //    refreshJustTotals = true;
+
+                } else {
+                    // It appears this was an actual modification and not a recursive notification.
+                    // doFullRefresh = true;
+                    blueprintify();
+                }
+            }
+
+        }
+
+    });
+
+
 });
+
+const observerConfiguration = {
+    childList: true,
+    characterData: true,
+    attributes: false,
+    subtree: true
+};
+
+listObserver.observe( document.body, observerConfiguration );
